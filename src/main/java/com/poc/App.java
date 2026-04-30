5e5434cfacf687514a05491ca0d05832f1e9ce7a
Humaira Hashmi <137989048+humaira0316@users.noreply.github.com>
2026-04-30 09:54:55 +0530

    Create App.java

---
+++ src/main/java/com/poc/App.java
@@ -0,0 +1,8 @@
+package com.poc;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("POC-1: Maven Build, Dockerize and Push to Docker Hub");
+    }
+}
+``
